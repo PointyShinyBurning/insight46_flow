181e78a0a9869789535b6fa2f63481651ae0494d
DanielKey <PointyShinyBurning@gmail.com>
2018-02-13 16:24:43 +0000

    Test subdag factory method without import

--- insight46_flow.py
+++ insight46_flow.py
@@ -1,6 +1,5 @@
 from airflow import DAG
 from cpgintegrate.connectors import OpenClinica, XNAT
-from cpgintegrate.airflow import dataset_list_subdag
 from datetime import datetime
 # from cpgintegrate.airflow.cpg_airflow_plugin import CPGDatasetToXCom, XComDatasetToCkan
 from airflow.operators.cpg_plugin import CPGDatasetToXCom, XComDatasetToCkan
@@ -16,6 +15,20 @@ default_args = {
     'email_on_retry': True,
     'start_date': START_DATE,
 }
+
+
+def dataset_list_subdag(dag_id, start_date, connector_class, connection_id, ckan_connection_id, ckan_package_id, pool,
+                        dataset_list):
+    subdag = DAG(dag_id, start_date=start_date)
+    with subdag as dag:
+        for dataset in dataset_list:
+            pull = CPGDatasetToXCom(task_id=dataset, connector_class=connector_class, connection_id=connection_id,
+                                    dataset_args=[dataset], pool=pool)
+            push = XComDatasetToCkan(task_id=dataset + '_ckan_push',
+                                     ckan_connection_id=ckan_connection_id, ckan_package_id=ckan_package_id)
+            pull >> push
+    return subdag
+
 
 insight = DAG('insight46_flow', default_args=default_args, schedule_interval='1 0 * * *')
 with insight as dag:
