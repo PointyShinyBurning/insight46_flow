9f49dea5ee81ac3f1ae71c349e0639870b539839
DanielKey <PointyShinyBurning@gmail.com>
2018-02-13 16:53:09 +0000

    Bump cpgintegrate version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="insight46_flow",
     install_requires=[
-        'cpgintegrate>=0.2.12',
+        'cpgintegrate>=0.2.13',
         'apache-airflow>=1.9.0',
     ],
 )
