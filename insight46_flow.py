03518d83dee6f7381dad39a86ca756a38c0d70fb
DanielKey <PointyShinyBurning@gmail.com>
2018-02-13 15:12:45 +0000

    Use new SubDagOperator subclass

--- insight46_flow.py
+++ insight46_flow.py
@@ -3,7 +3,7 @@ from cpgintegrate.connectors import OpenClinica, XNAT
 from cpgintegrate.airflow.dag_maker import dataset_list_to_ckan
 from datetime import datetime
 # from cpgintegrate.airflow.cpg_airflow_plugin import CPGDatasetToXCom, XComDatasetToCkan
-from airflow.operators.cpg_plugin import CPGDatasetToXCom, XComDatasetToCkan
+from airflow.operators.cpg_plugin import CPGDatasetToXCom, XComDatasetToCkan, CPGDatasetListToCkan
 
 default_args = {
     'owner': 'airflow',
@@ -15,26 +15,28 @@ default_args = {
 
 insight = DAG('insight46_flow', start_date=datetime(2018, 2, 11), schedule_interval='1 0 * * *',
               default_args=default_args)
-
-dataset_list_to_ckan(insight, OpenClinica, 'insight46_openclinica', 'ckan', 'insight46_admin', 'openclinica',
-                     dataset_list=[
-                         'F_3DECHOANALYS',
-                         'F_CIMTDONE',
-                         'F_ECGSABREV3',
-                         'F_ECHODONE',
-                         'F_FINGERTAPPIN',
-                         'F_HEARTRHYTHM',
-                         'F_IMAGEQUALITY',
-                         'F_INCIDENTALFI_2099',
-                         'F_OXYPROJ',
-                         'F_SPHYGMACOR',
-                         'F_SPOONS',
-                         'F_STROOP',
-                         'F_TRAILMAKING',
-                         'F_URINECOLLECT',
-                         'F_VALVEDISORDE',
-                         'F_VICORDERFILE'
-                     ])
 with insight as dag:
-    CPGDatasetToXCom(task_id='XNAT_SESSIONS', connector_class=XNAT, connection_id="insight46_xnat")\
-        >> XComDatasetToCkan(task_id='XNAT_SESSIONS_push_to_ckan', ckan_connection_id='ckan', ckan_package_id='insight46_admin')
+    CPGDatasetListToCkan(task_id="AllOpenClinica", connector_class=OpenClinica, connection_id='insight46_openclinica',
+                         ckan_connection_id='ckan', ckan_package_id='insight46_admin', pool='openclinica',
+                         dataset_list=[
+                             'F_3DECHOANALYS',
+                             'F_CIMTDONE',
+                             'F_ECGSABREV3',
+                             'F_ECHODONE',
+                             'F_FINGERTAPPIN',
+                             'F_HEARTRHYTHM',
+                             'F_IMAGEQUALITY',
+                             'F_INCIDENTALFI_2099',
+                             'F_OXYPROJ',
+                             'F_SPHYGMACOR',
+                             'F_SPOONS',
+                             'F_STROOP',
+                             'F_TRAILMAKING',
+                             'F_URINECOLLECT',
+                             'F_VALVEDISORDE',
+                             'F_VICORDERFILE'
+                         ])
+
+    CPGDatasetToXCom(task_id='XNAT_SESSIONS', connector_class=XNAT, connection_id="insight46_xnat") >> \
+        XComDatasetToCkan(task_id='XNAT_SESSIONS_push_to_ckan', ckan_connection_id='ckan',
+                          ckan_package_id='insight46_admin')
